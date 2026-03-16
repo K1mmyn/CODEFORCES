7dac050f55633191ca4f940c88ee056c8b75cf26
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2026-03-16 22:23:06 +1300

    CF807-D2-A correct

--- Training 2026/CF807-D2-A.py
+++ Training 2026/CF807-D2-A.py
@@ -1,10 +1,25 @@
 n = int(input())
 
-res = "unrated"
-for _ in range(n):
-    a, b = list(map(int, input().split(" ")))
-    if a != b:
-        res = "rated"
-        break
+res = "maybe"
 
-print(res)
+prev = float("inf")
+flag1 = False
+flag2 = False
+
+for i in range(n):
+    a, b = list(map(int, input().split(" ")))
+
+    if a != b:
+        flag1 = True
+        break
+    elif a > prev:
+        flag2 = True
+
+    prev = a
+
+if flag1:
+    print("rated")
+elif flag2:
+    print("unrated")
+else:
+    print("maybe")
